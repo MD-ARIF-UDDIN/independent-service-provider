15958f3b5ac121af72b29804eab833ffd131176d
MD-ARIF-UDDIN <mdarifuddin040@gmail.comm>
2022-04-18 23:45:04 +0600

    final commit done

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -6,7 +6,7 @@ import "react-toastify/dist/ReactToastify.css";
 const Checkout = () => {
   const { serviceId } = useParams();
   const submitBook = async () => {
-    toast("Thanks for booking");
+    toast("Thanks for booking.");
   };
   return (
     <div className="service-details">
